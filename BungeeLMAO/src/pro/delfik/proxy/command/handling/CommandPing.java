4c8f7dfc7070fa5721045f297c531a4f86198894
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-07-15 01:14:25 +0300

    /ping облагорожен.

--- BungeeLMAO/src/pro/delfik/proxy/command/handling/CommandPing.java
+++ BungeeLMAO/src/pro/delfik/proxy/command/handling/CommandPing.java
@@ -7,12 +7,14 @@ import pro.delfik.proxy.permissions.Rank;
 
 public class CommandPing extends Command{
 
-    public CommandPing(){
-        super("ping", Rank.PLAYER, "TODO");//TODO
-    }
+	public CommandPing() {
+		super("ping", Rank.PLAYER, "Проверить пинг игрока");
+	}
 
-    @Override
-    protected void run(CommandSender sender, String[] args) {
-        msg(sender, ((ProxiedPlayer)sender).getPing());//TODO
-    }
+	@Override
+	protected void run(CommandSender sender, String[] args) {
+		ProxiedPlayer victim = args.length == 0 ? ((ProxiedPlayer) sender) : requirePlayer(args[0]);
+		msg(sender, "§a[§f", victim, "§a] Пинг до прокси - §f" + victim.getPing() + "§aмс.");
+		msg(sender, "§a[§f", victim, "§a] Пинг до @" + victim.getServer().getInfo().getName() + " - §fнеизвестен§a.");
+	}
 }
