79099d0fca21ffa9d2965b18097beb121858dc5c
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-07-15 03:52:43 +0300

    Облагораживаем команду

--- BungeeLMAO/src/pro/delfik/proxy/command/handling/CommandFM.java
+++ BungeeLMAO/src/pro/delfik/proxy/command/handling/CommandFM.java
@@ -1,25 +1,39 @@
 package pro.delfik.proxy.command.handling;
 
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import pro.delfik.proxy.command.Command;
 import pro.delfik.proxy.permissions.Rank;
+
+import java.util.HashMap;
 
 public class CommandFM extends Command{
 	public CommandFM(String command, String description, String... aliases){
 		super(command, Rank.PLAYER, description, aliases);
 	}
 
+	
+	private static final HashMap<String, Rule> rules = new HashMap<>();
+	static {
+		rules.put("osk", new Rule("Оскорбление", 120));
+		rules.put("flood", new Rule("Флуд", 30));
+		rules.put("mat", new Rule("Мат", 60));
+	}
+	
 	@Override
 	protected void run(CommandSender sender, String[] args) {
 		requireArgs(args, 1, "[Игрок]");
 		String command = getCommand();
-		if(command.equals("osk")){
-			((ProxiedPlayer)sender).chat("/mute " + args[0] + " 120 1234");//TODO
-		}else if(command.equals("flud")){
-			((ProxiedPlayer)sender).chat("/mute " + args[0] + " 30 1234");
-		}else if(command.equals("mat")){
-			((ProxiedPlayer)sender).chat("/mute " + args[0] + " 60 1234");
+		Rule rule = rules.get(command);
+		Mutes.mute(args[0], rule.reason, rule.time, sender.getName());
+	}
+	
+	private static class Rule {
+		private final String reason;
+		private final int time;
+		
+		public Rule(String reason, int time) {
+			this.reason = reason;
+			this.time = time;
 		}
 	}
 }
