10b20cdb64c08b8aa0c0404f12efaba4908624ac
6oogle <32604831+6oogle@users.noreply.github.com>
2018-07-15 03:43:27 +0300

    Добавил странную команду

--- BungeeLMAO/src/pro/delfik/proxy/command/handling/CommandFM.java
+++ BungeeLMAO/src/pro/delfik/proxy/command/handling/CommandFM.java
@@ -1,6 +1,7 @@
 package pro.delfik.proxy.command.handling;
 
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import pro.delfik.proxy.command.Command;
 import pro.delfik.proxy.permissions.Rank;
 
@@ -11,9 +12,14 @@ public class CommandFM extends Command{
 
 	@Override
 	protected void run(CommandSender sender, String[] args) {
+		requireArgs(args, 1, "[Игрок]");
 		String command = getCommand();
 		if(command.equals("osk")){
-			sender.
+			((ProxiedPlayer)sender).chat("/mute " + args[0] + " 120 1234");//TODO
+		}else if(command.equals("flud")){
+			((ProxiedPlayer)sender).chat("/mute " + args[0] + " 30 1234");
+		}else if(command.equals("mat")){
+			((ProxiedPlayer)sender).chat("/mute " + args[0] + " 60 1234");
 		}
 	}
 }
