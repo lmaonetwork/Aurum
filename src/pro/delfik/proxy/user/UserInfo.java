cb70313da47207705ae4db93c449a4248448a49b
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-09-12 16:45:56 +0300

    [Rewrite] Переписан под систему версий

--- src/pro/delfik/proxy/user/UserInfo.java
+++ src/pro/delfik/proxy/user/UserInfo.java
@@ -1,76 +1,94 @@
 package pro.delfik.proxy.user;
 
+import implario.util.ByteUnzip;
+import implario.util.ManualByteZip;
 import implario.util.Rank;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class UserInfo {
-	private boolean authorized = false;
-	public String name;
-	public String password = "";
-	public int money = 0;
-	public Rank rank = Rank.PLAYER;
-	public long online = 0;
-	public String ip;
-	public boolean ipbound;
-	public List<String> ignoredPlayers;
-	public boolean pmDisabled;
-	public List<String> friends;
-	
-	public UserInfo(String name) {this.name = name;}
-	
-	public UserInfo(String name, String password, int money, Rank rank, long online, String ip, boolean ipbound,
-					List<String> ignoredPlayers, boolean pmDisabled, List<String> friends) {
+	public int version;
+
+	public String name, passhash, lastIP;
+	public boolean ipAttached, pmDisabled, darkTheme;
+	public Rank rank;
+	public int online, money;
+	public List<String> friends, ignored;
+
+	public UserInfo(String name, String passhash, Rank rank, int online, String lastIP,
+					int money, boolean ipAttached, boolean pmDisabled, List<String> ignored,
+					List<String> friends, boolean darkTheme) {
 		this.name = name;
-		this.password = password;
-		this.money = money;
+		this.passhash = passhash;
 		this.rank = rank;
 		this.online = online;
-		this.ip = ip;
-		this.ipbound = ipbound;
-		this.ignoredPlayers = ignoredPlayers;
+		this.lastIP = lastIP;
+		this.money = money;
+		this.ipAttached = ipAttached;
 		this.pmDisabled = pmDisabled;
+		this.ignored = ignored;
 		this.friends = friends;
-	}
-	
-	public Rank getRank() {
-		return rank;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getMoney() {
-		return money;
-	}
-	
-	public long getOnline() {
-		return online;
-	}
-	
-	public String getIp() {
-		return ip;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public boolean isIPBound() {
-		return ipbound;
+		this.darkTheme = darkTheme;
 	}
 
-	public boolean isPmDisabled() {
-		return pmDisabled;
+	public ManualByteZip zip() {
+		return new ManualByteZip()
+				.add(Version.last())
+				.add(name)
+				.add(passhash)
+				.add(rank.getByte())
+				.add(online)
+				.add(lastIP)
+				.add(money)
+				.add(ipAttached)
+				.add(pmDisabled)
+				.add(ignored)
+				.add(friends)
+				.add(darkTheme);
 	}
-	
-	public List<String> getFriends() {
-		return friends == null ? new ArrayList<>() : friends;
-	}
-	
-	public List<String> getIgnoredPlayers() {
-		return ignoredPlayers == null ? new ArrayList<>() : ignoredPlayers;
+
+	public enum Version {
+		V0(u -> new UserInfo(
+				u.getString(),
+				u.getString(),
+				Rank.byChar.get((char) u.getByte()),
+				u.getInt(),
+				u.getString(),
+				u.getInt(),
+				u.getBoolean(),
+				u.getBoolean(),
+				u.getList(),
+				u.getList(),
+				false
+		)),
+		V1_DARKTHEME(u -> new UserInfo(
+				u.getString(),
+				u.getString(),
+				Rank.byChar.get((char) u.getByte()),
+				u.getInt(),
+				u.getString(),
+				u.getInt(),
+				u.getBoolean(),
+				u.getBoolean(),
+				u.getList(),
+				u.getList(),
+				u.getBoolean()
+		));
+
+		private final Function<ByteUnzip, UserInfo> unzipper;
+
+		Version(Function<ByteUnzip, UserInfo> unzipper) {
+			this.unzipper = unzipper;
+		}
+
+		public static UserInfo unzip(ByteUnzip unzip) {
+			Version v = Version.values()[unzip.getInt()];
+			return v.unzipper.apply(unzip);
+		}
+
+		public static int last() {
+			return values().length - 1;
+		}
 	}
 }
