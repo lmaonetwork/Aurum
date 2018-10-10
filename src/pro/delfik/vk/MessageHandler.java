b830e0d8d9335e40e8b8c197633006203b4e0926
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-10-11 00:11:21 +0300

    [Del] Обработка ipchange и confirm

--- src/pro/delfik/vk/MessageHandler.java
+++ src/pro/delfik/vk/MessageHandler.java
@@ -1,47 +1,41 @@
 package pro.delfik.vk;
 
-import pro.delfik.proxy.cmd.user.CmdVK.PageAttachRequest;
-import pro.delfik.proxy.data.Database;
-import pro.delfik.proxy.User;
-import implario.util.Converter;
-
-import java.sql.ResultSet;
-
 public class MessageHandler {
 	public static String[] handle(String text, int from_id, long peer_id) {
 		String ask = text.toLowerCase().replace(",", "");
-		if (ask.startsWith("ipchange")) {
-			String name = null;
-			try {
-				Database.Result result = Database.sendQuery("SELECT nickname FROM VKPages WHERE link = '" + from_id + "'");
-				ResultSet set = result.set;
-				if (set.next()) {
-					name = set.getString("nickname");
-				}
-				result.st.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new String[] {"При обработке запроса произошла ошибка :c"};
-			}
-			
-			if (name == null) {
-				return new String[] {VK.getUserName(from_id) + ", к вашей странице VK не прикреплён игровой аккаунт, ",
-						"Чтобы прикрепить страницу, введите команду /vk в игре."};
-			}
-			User.allowedIP.add(name.toLowerCase());
-			return new String[] {"С аккаунта " + name + " снята привязка IP-адреса. У вас есть одна минута, затем защита восстановится."};
-		}
-		if (ask.startsWith("confirm ")) {
-			String codeStr = ask.substring(8);
-			int code = Converter.toInt(codeStr, -1);
-			if (code < 1) return new String[] {VK.getUserName(from_id) + ", код подтверждения првязки страницы VK введён неверно."};
-			PageAttachRequest request = PageAttachRequest.byCode.get(code);
-			if (request == null || request.getPageID() != from_id)
-				return new String[] {VK.getUserName(from_id) + ", код потверждения введён неверно. Это значит, что либо вы ещё не ввели команду /vk на сервере, " +
-											 "либо вы ошиблись, когда вводили sID страницы/Код подтверждения.", "Попробуйте ещё раз."};
-			request.confirm();
-			return new String[] {VK.getUserName(from_id) + ", аккаунт " + request.getPlayer() + " успешно привязан к вашей странице ВКонтакте!"};
-		}
+		if (text.equals("\uD83C\uDF6A")) return new String[]{"Вкусная печенька, спасибо!"};
+//		if (ask.startsWith("ipchange")) {
+//			String name = null;
+//			try {
+//				Database.Result result = Database.sendQuery("SELECT nickname FROM VKPages WHERE link = '" + from_id + "'");
+//				ResultSet set = result.set;
+//				if (set.next()) {
+//					name = set.getString("nickname");
+//				}
+//				result.st.close();
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//				return new String[] {"При обработке запроса произошла ошибка :c"};
+//			}
+//
+//			if (name == null) {
+//				return new String[] {VK.getUserName(from_id) + ", к вашей странице VK не прикреплён игровой аккаунт, ",
+//						"Чтобы прикрепить страницу, введите команду /vk в игре."};
+//			}
+//			User.allowedIP.add(name.toLowerCase());
+//			return new String[] {"С аккаунта " + name + " снята привязка IP-адреса. У вас есть одна минута, затем защита восстановится."};
+//		}
+//		if (ask.startsWith("confirm ")) {
+//			String codeStr = ask.substring(8);
+//			int code = Converter.toInt(codeStr, -1);
+//			if (code < 1) return new String[] {VK.getUserName(from_id) + ", код подтверждения првязки страницы VK введён неверно."};
+//			PageAttachRequest request = PageAttachRequest.byCode.get(code);
+//			if (request == null || request.getPageID() != from_id)
+//				return new String[] {VK.getUserName(from_id) + ", код потверждения введён неверно. Это значит, что либо вы ещё не ввели команду /vk на сервере, " +
+//											 "либо вы ошиблись, когда вводили sID страницы/Код подтверждения.", "Попробуйте ещё раз."};
+//			request.confirm();
+//			return new String[] {VK.getUserName(from_id) + ", аккаунт " + request.getPlayer() + " успешно привязан к вашей странице ВКонтакте!"};
+//		}
 		return new String[0];
 		
 	}
