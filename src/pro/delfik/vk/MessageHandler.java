ebddcdc31bca0be3a009cb1a6f3e34d24bef6aa5
oogle <anartvel@gmail.com>
2018-10-29 00:34:01 +0300

    ._.

--- src/pro/delfik/vk/MessageHandler.java
+++ src/pro/delfik/vk/MessageHandler.java
@@ -1,42 +1,20 @@
 package pro.delfik.vk;
 
+import net.md_5.bungee.api.config.ServerInfo;
+import pro.delfik.proxy.Proxy;
+
 public class MessageHandler {
 	public static String[] handle(String text, int from_id, long peer_id) {
-		String ask = text.toLowerCase().replace(",", "");
 		if (text.equals("\uD83C\uDF6A")) return new String[]{"Вкусная печенька, спасибо!"};
-//		if (ask.startsWith("ipchange")) {
-//			String name = null;
-//			try {
-//				Database.Result result = Database.sendQuery("SELECT nickname FROM VKPages WHERE link = '" + from_id + "'");
-//				ResultSet set = result.set;
-//				if (set.next()) {
-//					name = set.getString("nickname");
-//				}
-//				result.st.close();
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				return new String[] {"При обработке запроса произошла ошибка :c"};
-//			}
-//
-//			if (name == null) {
-//				return new String[] {VK.getUserName(from_id) + ", к вашей странице VK не прикреплён игровой аккаунт, ",
-//						"Чтобы прикрепить страницу, введите команду /vk в игре."};
-//			}
-//			User.allowedIP.add(name.toLowerCase());
-//			return new String[] {"С аккаунта " + name + " снята привязка IP-адреса. У вас есть одна минута, затем защита восстановится."};
-//		}
-//		if (ask.startsWith("confirm ")) {
-//			String codeStr = ask.substring(8);
-//			int code = Converter.toInt(codeStr, -1);
-//			if (code < 1) return new String[] {VK.getUserName(from_id) + ", код подтверждения првязки страницы VK введён неверно."};
-//			PageAttachRequest request = PageAttachRequest.byCode.get(code);
-//			if (request == null || request.getPageID() != from_id)
-//				return new String[] {VK.getUserName(from_id) + ", код потверждения введён неверно. Это значит, что либо вы ещё не ввели команду /vk на сервере, " +
-//											 "либо вы ошиблись, когда вводили sID страницы/Код подтверждения.", "Попробуйте ещё раз."};
-//			request.confirm();
-//			return new String[] {VK.getUserName(from_id) + ", аккаунт " + request.getPlayer() + " успешно привязан к вашей странице ВКонтакте!"};
-//		}
+		else if(text.equalsIgnoreCase("!Онлайн")) {
+			ServerInfo infos[] = Proxy.i().getServers().values().toArray(new ServerInfo[]{});
+			String result = "Общий онлайн: " + Proxy.i().getPlayers().size();
+			for(int i = 0; i < infos.length; i++){
+				ServerInfo info = infos[i];
+				result = result + "\r\n" + info.getName() + ": " + info.getPlayers().size();
+			}
+			return new String[]{result};
+		}
 		return new String[0];
-		
 	}
 }
