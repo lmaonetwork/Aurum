ac3dfe29c88bdb362ec0181c7c5ab584c31d4b21
oogle <anartvel@gmail.com>
2018-11-01 03:37:13 +0300

    add setrank

--- src/pro/delfik/vk/cmd/CmdAurum.java
+++ src/pro/delfik/vk/cmd/CmdAurum.java
@@ -1,6 +1,12 @@
 package pro.delfik.vk.cmd;
 
+import implario.util.Rank;
+import implario.util.UserInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import pro.delfik.proxy.User;
+import pro.delfik.proxy.cmd.Command;
 import pro.delfik.proxy.modules.Chat;
+import pro.delfik.util.Logger;
 import pro.delfik.vk.module.Admin;
 
 public class CmdAurum extends Cmd{
@@ -17,6 +23,23 @@ public class CmdAurum extends Cmd{
             return "Removed mat " + args[1];
         }else if(key.equalsIgnoreCase("checkMat")){
             return Chat.applyMat(args[1]);
+        }else if(key.equalsIgnoreCase("setRank")){
+            try {
+                Rank rank = Command.requireRank(args[1]);
+                Logger.log("Perm", "vk_" + id + " set " + rank + " " + args[0]);
+                User u = User.get(args[0]);
+                if (u != null) {
+                    u.setRank(rank);
+                    return "§aИгроку §f" + args[0] + "§a был выдан ранг §f" + rank.represent();
+                }
+                UserInfo info = User.read(args[0]);
+                if (info == null) return "§cИгрок §e" + args[0] + "§c не зарегистрирован.";
+                info.rank = rank;
+                User.save(info);
+                return "§aОффлайн-игроку §f" + args[0] + "§a был выдан ранг §f" + rank.represent();
+            }catch (Throwable ex){
+                return "Error";
+            }
         }
         return "Subcommand not found";
     }
